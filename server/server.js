284a06df9d64b247756b46eb820d37c2eab14e83
Piyush Mehta <me@piyushmehta.com>
2018-07-20 02:19:16 +0530

    Sockets io
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
 const publicPath = path.join(__dirname,'../public');
+const socketIO = require('socket.io');
 var app = express();
+var server = http.createServer(app);
 const port = process.env.PORT || 3000;
+var io = socketIO(server);
 app.use(express.static(publicPath));
 app.listen(port,()=>{
   console.log(`Server is up and Running on ${port}`);
